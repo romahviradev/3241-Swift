1fdabf73d8a65b90d32831b22afd16ea58ab95d2
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-10-24 16:29:54 +0300

    init

---
+++ EggAndFit/App/EggAndFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  EggAndFitApp.swift
+//  EggAndFit
+//
+//  Created by Serhii Babchuk on 24.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct EggAndFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            EALaunchView()
+        }
+    }
+}
